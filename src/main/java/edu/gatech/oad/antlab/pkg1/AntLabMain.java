b821edd14f3c0131a9aee0d48dc366932202eb20
Johnny <leejohnny47@gmail.com>
2017-01-31 23:03:22 -0500

    Fixed my person file and someone deleted the print statement in the main class

--- src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
+++ src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
@@ -76,6 +76,7 @@ import edu.gatech.oad.antlab.pkg2.*;
       //and gburdell5 with your gt id
       Person5 p5 = new Person5("P5");
       toPrint += p5.toString("gburdell5");
+      System.out.println(toPrint);
     }
 
 
