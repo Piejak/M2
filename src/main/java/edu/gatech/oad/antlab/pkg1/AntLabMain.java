a629cd354a8529e22a4a953e33b713e9d3b7252f
Jimmy D <Jimmy D>
2017-02-01 12:55:27 -0500

    added person 5 antlabmain

--- src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
+++ src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
@@ -74,8 +74,8 @@ import edu.gatech.oad.antlab.pkg2.*;
       toPrint += p4.toString("hli615");
       //Person5 replace P4 with your name
       //and gburdell5 with your gt id
-      Person5 p5 = new Person5("P5");
-      toPrint += p5.toString("gburdell5");
+      Person5 p5 = new Person5("Jimmy Dinh-Nguyen");
+      toPrint += p5.toString("jpdn3");
       System.out.println(toPrint);
     }
 
